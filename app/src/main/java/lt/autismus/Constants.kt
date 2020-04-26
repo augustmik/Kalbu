8e14d9d3ae80f7244bb0a533029ddc1d7abb5933
Augustas Mikulenas <augustmik@gmail.com>
2020-04-26 15:07:39 +0300

    adjusted dim to be a bit darker

--- app/src/main/java/lt/autismus/Constants.kt
+++ app/src/main/java/lt/autismus/Constants.kt
@@ -5,4 +5,4 @@ const val TAKE_PICTURE_REQUEST_CODE = 1001
 const val CANCEL_TIMER : Long = 3000 //3 seconds
 const val TIMER_TICK : Long = 100 //0.1 seconds
 
-const val BIGGER_DIM_AMOUNT : Float = 0.8f //0.0f = no dim, 1.0f = full black
+const val BIGGER_DIM_AMOUNT : Float = 0.95f //0.0f = no dim, 1.0f = full black
