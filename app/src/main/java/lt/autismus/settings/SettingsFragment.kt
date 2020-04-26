5f9f4d6279e976cc00849f15ed21c66e3794152f
Augustas Mikulenas <augustmik@gmail.com>
2020-04-26 16:10:02 +0300

    added imbedded chrome browser, download icons setting

--- app/src/main/java/lt/autismus/settings/SettingsFragment.kt
+++ app/src/main/java/lt/autismus/settings/SettingsFragment.kt
@@ -1,6 +1,9 @@
 package lt.autismus.settings
 
+import android.net.Uri
 import android.os.Bundle
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.content.ContextCompat.getColor
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import lt.autismus.R
@@ -13,9 +16,18 @@ class SettingsFragment : PreferenceFragmentCompat() {
     }
 
     override fun onPreferenceTreeClick(preference: Preference?): Boolean {
+        if (preference?.key == "download_images") {
+            openCustomChromeTabsPage(Uri.parse(getString(R.string.add_more_images_link)))
+        }
         if (preference?.key == "about") {
             //TODO: handle preferences here
         }
         return super.onPreferenceTreeClick(preference)
+    }
+    private fun openCustomChromeTabsPage(page: Uri) {
+        val builder = CustomTabsIntent.Builder()
+        builder.setToolbarColor(getColor(requireContext(), R.color.colorPrimary))
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(requireContext(), page)
     }
 }
