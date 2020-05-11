873dc25ee00ad82675f2cd84e242b5e3acff3639
Augustas Mikulenas <augustmik@gmail.com>
2020-05-11 18:21:25 +0300

    added tutorial messages

--- app/src/main/java/lt/kalbu/repository/FirstLaunchLoader.kt
+++ app/src/main/java/lt/kalbu/repository/FirstLaunchLoader.kt
@@ -7,17 +7,24 @@ import lt.kalbu.R
 import lt.kalbu.singleUnits.SingleCard
 import lt.kalbu.singleUnits.SingleCategory
 
-
 class FirstLaunchLoader(val context: Context) {
+    private val preloadTutorial = "Apmokymai"
+    private val preloadFood = "Maistas"
+    private val preloadHome = "Namai"
+
     fun getCat() : List<SingleCategory>{
         //list of categories to add
         return mutableListOf(
             SingleCategory(
-                "Maistas",
+                preloadTutorial,
+                getDrawableUriString(R.drawable.tutorial_category)
+            ),
+            SingleCategory(
+                preloadFood,
                 getDrawableUriString(R.drawable.preload_food)
             ),
             SingleCategory(
-                "Namai",
+                preloadHome,
                 getDrawableUriString(R.drawable.preload_home)
             )
         )
@@ -27,44 +34,64 @@ class FirstLaunchLoader(val context: Context) {
         //list of cards to add
         return mutableListOf(
             SingleCard(
+                image = getDrawableUriString(R.drawable.tutorial_enlarge),
+                title = "Kortelės didinimas",
+                category = preloadTutorial
+            ),
+            SingleCard(
+                image = getDrawableUriString(R.drawable.tutorial_settings),
+                title = "Aktyvuoti visą funkcionalumą",
+                category = preloadTutorial
+            ),
+            SingleCard(
+                image = getDrawableUriString(R.drawable.tutorial_create_card),
+                title = "Kortelių ar Kategorijų kūrimas",
+                category = preloadTutorial
+            ),
+            SingleCard(
+                image = getDrawableUriString(R.drawable.tutorial_delete),
+                title = "Kortelių ar Kategorijų naikinimas",
+                category = preloadTutorial
+            ),
+            SingleCard(
                 image = getDrawableUriString(R.drawable.preload_food_apple),
                 title = "Obuolys",
-                category = "Maistas"
+                category = preloadFood
             ),
             SingleCard(
                 image = getDrawableUriString(R.drawable.preload_food_banana),
                 title = "Bananas",
-                category = "Maistas"
+                category = preloadFood
             ),
             SingleCard(
                 image = getDrawableUriString(R.drawable.preload_food_sandwich),
                 title = "Sumuštinis",
-                category = "Maistas"
+                category = preloadFood
             ),
             SingleCard(
                 image = getDrawableUriString(R.drawable.preload_food_soup),
                 title = "Sriuba",
-                category = "Maistas"
+                category = preloadFood
             ),
             SingleCard(
                 image = getDrawableUriString(R.drawable.preload_home_bathtub),
                 title = "Vonia",
-                category = "Namai"
+                category = preloadHome
             ),
             SingleCard(
                 image = getDrawableUriString(R.drawable.preload_home_bed),
                 title = "Lova",
-                category = "Namai"
+                category = preloadHome
             ),
             SingleCard(
                 image = getDrawableUriString(R.drawable.preload_home_fridge),
                 title = "Šaldytuvas",
-                category = "Namai"
+                category = preloadHome
             ),
             SingleCard(
                 image = getDrawableUriString(R.drawable.preload_home_toilet),
                 title = "Tualetas",
-                category = "Namai"
+                category = preloadHome
             )
         )
     }
