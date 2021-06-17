ee4af71b8c984b388b71de214d6ae1997e010906
Augustas Mikulenas <augustmik@gmail.com>
2021-06-17 19:24:45 +0300

    preloaded categories

--- app/src/main/java/lt/kalbu/repository/FirstLaunchLoader.kt
+++ app/src/main/java/lt/kalbu/repository/FirstLaunchLoader.kt
@@ -9,8 +9,24 @@ import lt.kalbu.singleUnits.SingleCategory
 
 class FirstLaunchLoader(val context: Context) {
     private val preloadTutorial = "Naudojimo paaiškinimai\n(Išmokus ištrink mane!)"
+
+    // New ones
+    private val preloadCommonTasks = "Bendrieji veiksmai"
+    private val preloadMusic = "Muzika"
+    private val preloadGamesAndToys = "Žaidimai ir Žaislai"
+    private val preloadSport = "Sportas"
+    private val preloadToolsAndKitchenware = "Įrankiai"
+    private val preloadAnimals = "Gyvūnai"
+    private val preloadClothing = "Apranga"
+    private val preloadEmotions = "Emocijos"
     private val preloadFood = "Maistas"
     private val preloadHome = "Namai"
+    private val preloadPeople = "Žmonės"
+    private val preloadPlaces = "Vietos"
+    private val preloadTransport = "Transportas"
+    private val preloadColors = "Spalvos"
+    private val preloadShapes = "Formos"
+    private val preloadNumbers = "Skaičiai"
 
     fun getCat() : List<SingleCategory>{
         //list of categories to add
@@ -20,12 +36,68 @@ class FirstLaunchLoader(val context: Context) {
                 getDrawableUriString(R.drawable.tutorial_category)
             ),
             SingleCategory(
+                preloadCommonTasks,
+                getDrawableUriString(R.drawable.preload_category_commontasks)
+            ),
+            SingleCategory(
+                preloadMusic,
+                getDrawableUriString(R.drawable.preload_category_music)
+            ),
+            SingleCategory(
+                preloadGamesAndToys,
+                getDrawableUriString(R.drawable.preload_category_gamesntoys)
+            ),
+            SingleCategory(
+                preloadSport,
+                getDrawableUriString(R.drawable.preload_category_sports)
+            ),
+            SingleCategory(
+                preloadToolsAndKitchenware,
+                getDrawableUriString(R.drawable.preload_category_kitchenware)
+            ),
+            SingleCategory(
+                preloadAnimals,
+                getDrawableUriString(R.drawable.preload_category_animals)
+            ),
+            SingleCategory(
+                preloadClothing,
+                getDrawableUriString(R.drawable.preload_category_clothing)
+            ),
+            SingleCategory(
+                preloadEmotions,
+                getDrawableUriString(R.drawable.preload_category_emotions)
+            ),
+            SingleCategory(
                 preloadFood,
-                getDrawableUriString(R.drawable.preload_food)
+                getDrawableUriString(R.drawable.preload_category_food)
             ),
             SingleCategory(
                 preloadHome,
-                getDrawableUriString(R.drawable.preload_home)
+                getDrawableUriString(R.drawable.preload_category_home)
+            ),
+            SingleCategory(
+                preloadPeople,
+                getDrawableUriString(R.drawable.preload_category_people)
+            ),
+            SingleCategory(
+                preloadPlaces,
+                getDrawableUriString(R.drawable.preload_category_places)
+            ),
+            SingleCategory(
+                preloadTransport,
+                getDrawableUriString(R.drawable.preload_category_transport)
+            ),
+            SingleCategory(
+                preloadColors,
+                getDrawableUriString(R.drawable.preload_category_colors)
+            ),
+            SingleCategory(
+                preloadShapes,
+                getDrawableUriString(R.drawable.preload_category_shapes)
+            ),
+            SingleCategory(
+                preloadNumbers,
+                getDrawableUriString(R.drawable.preload_category_numbers)
             )
         )
     }
